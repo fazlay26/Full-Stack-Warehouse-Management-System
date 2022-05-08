49a44126902eee5a479b183666d50aa3793f10ae
fazlay26 <rabboi007@gmail.com>
2022-05-08 12:57:21 +0600

    update-env.local-file

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD4eyvLPhtJY6aYr0hbymsBmQrwQmoqwBA",
-    authDomain: "bike-manager-4766c.firebaseapp.com",
-    projectId: "bike-manager-4766c",
-    storageBucket: "bike-manager-4766c.appspot.com",
-    messagingSenderId: "399518309274",
-    appId: "1:399518309274:web:0a6b3040f5e3023d743823"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
